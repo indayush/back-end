2bffb7c7fc615ed64c3860741b7cd5c21ff04bb2
Ayush Adarsh <ayush.adarsh@healthgraph.in>
2023-07-07 15:56:49 +0530

    Added extra support files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
 module.exports = {
-	DB_URL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tijhe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+	DB_URL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.2t7oqrs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 };
